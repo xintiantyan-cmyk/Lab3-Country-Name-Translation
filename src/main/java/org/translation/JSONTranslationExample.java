5390f30948f627b966407048811f6601738cd65a
Tina Yan <tinayan@Tinas-MacBook-Air-5.local>
2026-05-21 20:02:21 -0400

    finished first TODO in JSONTranslationExample.java

--- src/main/java/org/translation/JSONTranslationExample.java
+++ src/main/java/org/translation/JSONTranslationExample.java
@@ -3,6 +3,7 @@ package org.translation;
 import java.io.IOException;
 import java.net.URISyntaxException;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 
 import org.json.JSONArray;
@@ -21,9 +22,10 @@ public class JSONTranslationExample {
         try {
             // this next line of code reads in a file from the resources folder as a String,
             // which we then create a new JSONArray object from.
-            // TODO CheckStyle: Line is longer than 120 characters
-            //                  (note: you can split a line such that the next line starts with a .method()... call
-            String jsonString = Files.readString(Paths.get(getClass().getClassLoader().getResource("sample.json").toURI()));
+            // Line is longer than 120 characters
+            // (note: you can split a line such that the next line starts with a .method()... call
+            Path a = Paths.get(getClass().getClassLoader().getResource("sample.json").toURI());
+            String jsonString = Files.readString(a);
             this.jsonArray = new JSONArray(jsonString);
         }
         catch (IOException | URISyntaxException ex) {
